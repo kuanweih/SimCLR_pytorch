680a8fe6cde069a694c81306691411a139fe3377
Kuan-Wei Huang <22647448+kuanweih@users.noreply.github.com>
2022-02-18 22:36:24 -0500

    added dataloader

---
+++ dataset_wrapper.py
@@ -0,0 +1,108 @@
+import cv2
+import numpy as np
+import torchvision.transforms as transforms
+
+from torchvision import datasets
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+
+
+np.random.seed(0)
+
+
+class DataSetWrapper(object):
+
+    def __init__(self, batch_size, num_workers, valid_size, input_shape, s):
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.valid_size = valid_size
+        self.s = s
+        self.input_shape = eval(input_shape)
+
+    def get_data_loaders(self):
+        data_augment = self._get_simclr_pipeline_transform()
+        train_dataset = datasets.STL10('./data', split='train+unlabeled',
+            download=True, transform=SimCLRDataTransform(data_augment))
+        train_loader, valid_loader = self.get_train_validation_data_loaders(train_dataset)
+        return train_loader, valid_loader
+
+    def _get_simclr_pipeline_transform(self):
+        # get a set of data augmentation transformations as described in the SimCLR paper.
+        color_jitter = transforms.ColorJitter(
+            0.8 * self.s, 0.8 * self.s, 0.8 * self.s, 0.2 * self.s)
+        data_transforms = transforms.Compose([
+            transforms.RandomResizedCrop(size=self.input_shape[0]),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomApply([color_jitter], p=0.8),
+            transforms.RandomGrayscale(p=0.2),
+            GaussianBlur(kernel_size=int(0.1 * self.input_shape[0])),
+            transforms.ToTensor()
+        ])
+        return data_transforms
+
+    def get_train_validation_data_loaders(self, train_dataset):
+        # obtain training indices that will be used for validation
+        num_train = len(train_dataset)
+        indices = list(range(num_train))
+        np.random.shuffle(indices)
+
+        split = int(np.floor(self.valid_size * num_train))
+        train_idx, valid_idx = indices[split:], indices[:split]
+
+        # define samplers for obtaining training and validation batches
+        train_sampler = SubsetRandomSampler(train_idx)
+        valid_sampler = SubsetRandomSampler(valid_idx)
+
+        train_loader = DataLoader(train_dataset, batch_size=self.batch_size,
+            sampler=train_sampler, num_workers=self.num_workers,
+            rop_last=True, shuffle=False)
+
+        valid_loader = DataLoader(train_dataset, batch_size=self.batch_size,
+            sampler=valid_sampler, num_workers=self.num_workers, drop_last=True)
+        return train_loader, valid_loader
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class SimCLRDataTransform():
+
+    def __init__(self, transform):
+        self.transform = transform
+
+    def __call__(self, sample):
+        xi = self.transform(sample)
+        xj = self.transform(sample)
+        return xi, xj
+
+
+class GaussianBlur():
+    # Implements Gaussian blur as described in the SimCLR paper
+    def __init__(self, kernel_size, min=0.1, max=2.0):
+        self.min = min
+        self.max = max
+        # kernel size is set to be 10% of the image height/width
+        self.kernel_size = kernel_size
+
+    def __call__(self, sample):
+        sample = np.array(sample)
+
+        # blur the image with a 50% chance
+        prob = np.random.random_sample()
+
+        if prob < 0.5:
+            sigma = (self.max - self.min) * np.random.random_sample() + self.min
+            sample = cv2.GaussianBlur(sample, (self.kernel_size, self.kernel_size), sigma)
+
+        return sample
